2b707c297e27ad0e4828c7bdcffa6e69bd0a375d
Luis Carvajal <114468881+LuisCarVal@users.noreply.github.com>
2025-02-19 09:35:17 +0100

    Update carvajalvalverde.sh

--- carvajalvalverde.sh
+++ carvajalvalverde.sh
@@ -37,7 +37,7 @@ backend webservers
 EOF
 
 echo "Creando archivo haproxy.cfg con $NUM_NODES nodos..."
-
+docker volume create carvajalvalverde
 for i in $(seq 1 $NUM_NODES); do
   PORT=$((BASE_PORT + i - 1))
   CONTAINER_NAME="carvajalvalverde-web$i"
@@ -47,7 +47,7 @@ for i in $(seq 1 $NUM_NODES); do
     -e PORT=$PORT \
     -e CONTAINER_NAME=$CONTAINER_NAME \
     -p $PORT:80 \
-    -v .:/var/www/html/carvajalvalverde \
+    -v carvajalvalverde:/var/www/html/carvajalvalverde \
     $IMAGE_NAME
 
   echo "  server $CONTAINER_NAME 130.61.90.52:$PORT check" >> $HAPROXY_CFG
